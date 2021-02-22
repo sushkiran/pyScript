db2618f52cf60f96cbbc13f6002c64fd28b94ace
sushkiran <sushvin@gmail.com>
2021-02-22 19:43:59 +0800

    Module3 Case Study 1 - Samples & Test code

--- module3/testMe.py
+++ module3/testMe.py
@@ -4,3 +4,18 @@ items = [1, 2, 3, 4, 5]
 print(list(map(lambda x: x**2, items)))
 print(reduce(lambda x, y: x * y, items))
 print(reduce(lambda x, y: x**2 * y**2, items))
+
+
+string = '630'
+print(string[:-2])
+print(string[-2:])
+
+string = '1630'
+print(string[:-2])
+print(string[-2:])
+
+
+i =10
+x = str(i)
+print(i)
+print(x[0])
