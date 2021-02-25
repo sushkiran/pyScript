f86c6fecf41bfe949644f9e98bbb7e206d6a4da3
sushkiran <sushvin@gmail.com>
2021-02-25 23:48:28 +0800

    Module 3 Case study 2 BankData.py

--- module3_case2/BankData.py
+++ module3_case2/BankData.py
@@ -12,3 +12,30 @@ Further Enhancements
 4.Currently program ends after the check.
 Take the input in while loop and end only if user types "END" for profession
 """
+
+import csv
+
+age_set = set()
+job_set = set()
+with open('bank-data.csv') as file:
+    reader = csv.reader(file)
+    head_row = next(reader)
+    for data_row in reader:
+        age_set.add(int(data_row[0]))
+        job_set.add(data_row[1])
+
+age_dict = {
+    'max': max(age_set),
+    'min': min(age_set)
+}
+print(job_set)
+print(age_dict)
+
+profession = ''
+while profession != 'END':
+    profession = input('Enter your profession: ')
+    text = 'Loan Eligibility {} for ' + profession
+    if profession.lower() in job_set:
+        print(text.format('APPROVED'))
+    else:
+        print(text.format('REJECTED'))
