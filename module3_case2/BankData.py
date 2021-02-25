036d9aeeff48ba4073ec988a84dbe973a56f71aa
sushkiran <sushvin@gmail.com>
2021-02-25 20:16:32 +0800

    Module 3 Case study 2 BankData.py

---
+++ module3_case2/BankData.py
@@ -0,0 +1,14 @@
+"""
+1.Read file bank-data.csv
+2.Build a set of unique jobs
+3.Read the input from command line –profession
+4.Check if profession is in list
+5.Print whether client is eligible
+
+Further Enhancements
+1.Compute max and min age for loan eligibility based on data in csv file
+2.Store max and min age in dictionary
+3.Make the profession check case insensitive
+4.Currently program ends after the check.
+Take the input in while loop and end only if user types "END" for profession
+"""
