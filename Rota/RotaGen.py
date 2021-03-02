782678dcc48e991c551dee692f9b7b5a68d4314b
sushkiran <sushvin@gmail.com>
2021-03-02 22:36:24 +0800

    Shift Rota Generation - Final delivery

--- Rota/RotaGen.py
+++ Rota/RotaGen.py
@@ -55,16 +55,27 @@ if __name__ == '__main__':
     group = df['Group'][0]
 
     for num in range(num_weeks):
+
         date += dt.timedelta(7)
         group = next_group(group)
 
         exclude_me = list(group_members[group])
+        if len(exclude_me) < 3:
+            prev_primary = df['Cal_Primary'][len(df.index) - 1]
+            exclude_me.append(prev_primary)
         primary = choose_member(exclude_me)
 
+        exclude_me = list(group_members[group])
         exclude_me.append(primary)
+        if len(exclude_me) < 3:
+            prev_bas = df['BAS_Finance'][len(df.index) - 1]
+            exclude_me.append(prev_bas)
         bas = choose_member(exclude_me)
 
         exclude_me = [primary, bas]
+        if len(exclude_me) < 3:
+            prev_standby = df['Cal_Standby'][len(df.index) - 1]
+            exclude_me.append(prev_standby)
         standby = choose_member(exclude_me)
 
         df.loc[len(df.index)] = [date, group, primary, standby, bas]
