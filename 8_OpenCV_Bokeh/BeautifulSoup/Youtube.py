e4d46102107538d4629ae5a8bc18bea899618172
sushkiran <sushvin@gmail.com>
2021-03-13 18:21:16 +0800

    Web Scraping Assignments + other stuff

--- 8_OpenCV_Bokeh/BeautifulSoup/Youtube.py
+++ 8_OpenCV_Bokeh/BeautifulSoup/Youtube.py
@@ -1,30 +1,19 @@
 """
 Write a program to download all the videos from youtube.com for django from the hyperlink given below
 """
-'''import requests
+import requests
 from bs4 import BeautifulSoup
 
-# link = 'https://www.youtube.com/playlist?list=PLxxA5z-8B2xk4szCgFmgonNcCboyNneMD'
-link = 'https://www.youtube.com/watch?v=oT1A1KKf0SI&list=PLxxA5z-8B2xk4szCgFmgonNcCboyNneMD&index=1'
+link = 'https://www.youtube.com/playlist?list=PLxxA5z-8B2xk4szCgFmgonNcCboyNneMD'
+
 content = requests.get(link).content
 soup = BeautifulSoup(content, "html.parser")
-print(soup.prettify())'''
 
-'''from pytube import YouTube
+print(soup.prettify())
+'''for tag in soup.find_all(True):
+    print()
+    print(tag)
 
-youtube_video_url = 'https://www.youtube.com/watch?v=DkU9WFj8sYo'
+print([tag.name for tag in soup.find_all('url')])'''
 
-yt_obj = YouTube(youtube_video_url)
 
-for stream in yt_obj.streams:
-    print(stream)'''
-
-from pytube import Playlist
-
-try:
-    playlist = Playlist('https://www.youtube.com/playlist?list=PLcow8_btriE11hzMbT3-B1sBg4YIc-9g_')
-
-    playlist.videos()
-
-except Exception as e:
-    print(e)
